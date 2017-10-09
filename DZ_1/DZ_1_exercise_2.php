51c55ab4d987a3fd2b78969c3446890359e9dc6f
Yaroslav <yaro7law@gmail.com>
2017-10-09 21:18:40 +0300

    Exercise 2

---
+++ DZ_1/DZ_1_exercise_2.php
@@ -0,0 +1,6 @@
+<?php
+$countPictures = 80;
+$picturesMarkers = 23;
+$picturePencils = 40;
+$pictureColors = $countPictures - ($picturesMarkers + $picturePencils);
+echo $pictureColors.' картин на школьной выставке выполненно красками';
