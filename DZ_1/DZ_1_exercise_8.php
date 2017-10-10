638388f84514a1fb48085ee52d3cc1c3d72ce56a
Yaroslav <yaro7law@gmail.com>
2017-10-11 00:28:37 +0300

    Add <br><hr> and add words Before / After in exercise 8

--- DZ_1/DZ_1_exercise_8.php
+++ DZ_1/DZ_1_exercise_8.php
@@ -1,6 +1,6 @@
 <?php
 $str = 'Hello Big World';
-//echo $str;
+echo 'Before : ' . $str . '<br><hr>';
 $str = explode(" ", $str);
 //print_r($str);
 $letter = count($str) - 1; // получаем колличество элементов в массиве и отнимаем 1, так как массив начинается с 0
@@ -10,7 +10,7 @@ while ($letter >=0) {
 }
 //print_r($rev_str);
 $rev_str = implode("***", $rev_str);
-echo $rev_str;
+echo 'After : ' . $rev_str;
 /**
  * Правильным ли было использовать count($str) - 1?
  */
