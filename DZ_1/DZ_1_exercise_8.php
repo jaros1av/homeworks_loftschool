8d45e59e47558ab0cccfcf658b19199c690b4988
Yaroslav <yaro7law@gmail.com>
2017-10-11 00:21:35 +0300

    Exercise 8

---
+++ DZ_1/DZ_1_exercise_8.php
@@ -0,0 +1,16 @@
+<?php
+$str = 'Hello Big World';
+//echo $str;
+$str = explode(" ", $str);
+//print_r($str);
+$letter = count($str) - 1; // получаем колличество элементов в массиве и отнимаем 1, так как массив начинается с 0
+while ($letter >=0) {
+    $rev_str[] = $str[$letter];
+    $letter--;
+}
+//print_r($rev_str);
+$rev_str = implode("***", $rev_str);
+echo $rev_str;
+/**
+ * Правильным ли было использовать count($str) - 1?
+ */
