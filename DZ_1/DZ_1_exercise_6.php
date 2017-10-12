b09691f1d362330a149a676d15de5a944e91bba5
Yaroslav <yaro7law@gmail.com>
2017-10-13 00:26:59 +0300

    DZ 1 exercise 6 via double foreach

--- DZ_1/DZ_1_exercise_6.php
+++ DZ_1/DZ_1_exercise_6.php
@@ -1,18 +1,28 @@
 <?php
-$bmw = ['name' => 'BMW', 'model' => 'X5', 'speed' => '120', 'doors' => '5', 'year' => '2015'];
-$toyota = ['name' => 'Toyota','model' => 'Corolla(E170)', 'speed' => '180', 'doors' => '4', 'year' => '2015'];
-$opel = ['name' => 'Opel', 'model' => 'Astra', 'speed' => '203', 'doors' => '4', 'year' => '2012'];
-$cars = [$bmw, $toyota, $opel];
+$bmw = ['model' => 'X5', 'speed' => '120', 'doors' => '5', 'year' => '2015'];
+$toyota = ['model' => 'Corolla(E170)', 'speed' => '180', 'doors' => '4', 'year' => '2015'];
+$opel = ['model' => 'Astra', 'speed' => '203', 'doors' => '4', 'year' => '2012'];
+$cars = ["BMW"=>$bmw, "Toyota"=>$toyota, "Opel"=>$opel];
+//echo "<pre>";
+//print_r($cars);
+//echo "</pre>";
 //$cars1 =  array_merge_recursive($bmw, $toyota, $opel);
 //echo "<pre>";
 //print_r($cars2);
 //echo "</pre>";
-$counts = count($cars);
-for ($i = 0; $i < $counts; $i++) {
-    echo '<p style ="border:1px solid black">' . $cars[$i]['name'] . '<br>' . $cars[$i]['model'] . ' - ' .
-        $cars[$i]['speed'] . ' - ' . $cars[$i]['doors'] . ' - ' . $cars[$i]['year'] . '</p>';
-}
+//$counts = count($cars);
+//for ($i = 0; $i < $counts; $i++) {
+//    echo '<p style ="border:1px solid black">' . $cars[$i]['name'] . '<br>' . $cars[$i]['model'] . ' - ' .
+//        $cars[$i]['speed'] . ' - ' . $cars[$i]['doors'] . ' - ' . $cars[$i]['year'] . '</p>';
+//}
 
+foreach ($cars as $car => $items) {
+    echo '<p style ="border:1px solid black">'. 'CAR ' . $car . '<br>';
+    foreach ($items as $item) {
+        echo '-'.$item;
+    }
+    echo '</p>';
+}
 /**
  * как лучше объединять массивы, через array_merge_recursive или как было сделано здесь? Просто array_merge заменяет
  * значения из схожих ключей, то есть на выходе получим один массив $opel
