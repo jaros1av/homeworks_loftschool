59033ee08a5279c71ad7738efcc0fefecefe2e03
Yaroslav <yaro7law@gmail.com>
2017-10-14 12:55:25 +0300

    Add exercise 3 DZ 2 and add question on the return

--- DZ_2/functions.php
+++ DZ_2/functions.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * return ведь необязательно должен быть указан?
+ * для избежания ошибки и вывода какого либо результата перед выходом из функции, можно указать return false?
+ *
+ *
+ */
 //Задание 1
 function task1($str_arr, $flag = false)
 {
@@ -10,8 +16,8 @@ function task1($str_arr, $flag = false)
             echo '<p>' . $item . '</p>';
         }
     }
+    return false;
 }
-
 //Задание 2
 function task2($num_arr, $action)
 {
@@ -52,8 +58,48 @@ function task2($num_arr, $action)
     } else {
         echo 'необходимо указать минимум два числа';
     }
+    return false;
 }
 //Задание 3
-function task3 () {
-
+function task3()
+{
+    $args = func_get_args();//массив параметров
+    $counts = count($args); // колличество параметров в массиве
+    $res = $args[1]; // получаем второй элемент массива
+    if ($counts >= 3) {
+        switch ($args[0]) {
+            case '+':
+                for ($i = 2; $i < $counts; $i++) { // начинаем со второго элемента, первый уже занесли в $res
+                    $res += $args[$i];
+                }
+                return $res;
+                break;
+            case '-':
+                for ($i = 2; $i < $counts; $i++) {
+                    $res -= $args[$i];
+                }
+                return $res;
+                break;
+            case '*':
+                for ($i = 2; $i < $counts; $i++) {
+                    $res *= $args[$i];
+                }
+                return $res;
+                break;
+            case '/':
+                for ($i = 2; $i < $counts; $i++) {
+                    if ($args[$i] == 0) {
+                        echo 'Делить на ноль нельзя!';
+                    }
+                    $res /= $args[$i];
+                }
+                return $res;
+                break;
+            default:
+                echo 'Неверно указана операция или операция указана не первым параметром!';
+        }
+    } else {
+        echo 'необходимо указать минимум три параметра';
+    }
+    return false;
 }
