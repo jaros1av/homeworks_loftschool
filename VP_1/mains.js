b4a1cd304a37e60bde450f58eca286ea0c7df7c6
Yaroslav <yaro7law@gmail.com>
2017-10-31 13:47:49 +0300

    second test VP1

--- VP_1/mains.js
+++ VP_1/mains.js
@@ -1,39 +1,17 @@
-$(document).ready(function(){
-    $('input[type=submit]').on('click', function() {
-        var name = $('input[name=name]').val();
-        var phone = $('input[name=phone]').val();
-        var email = $('input[name=email]').val();
-        var street = $('input[name=street]').val();
-        var home = $('input[name=home]').val();
-        var part = $('input[name=part]').val();
-        var appt = $('input[name=appt]').val();
-        var floor = $('input[name=floor]').val();
-        var comment = $('textarea[name=comment]').val();
-        var payment = $('input[type=radio]').val();
-        var callback = $('input[name=callback]').val();
-        // console.log(name, phone, email, street, home, part, appt, floor, comment, payment, callback);
-        $.post({
+$(document).ready(function() {
+    $('#order-form').on ('submit', function (e) {
+        e.preventDefault(); //перехват и отключение стандартных действий браузера
+        var data_form = $(this).serialize(); // забираем все из формы
+        $.post({ //  сразу отправка методом пост
             url: 'sendler.php',
-            data: {
-                name: name,
-                email: email,
-                phone: phone,
-                street: street,
-                home: home,
-                part: part,
-                appt: appt,
-                floor: floor,
-                comment: comment,
-                payment: payment,
-                callback: callback
+            data: data_form,
+            success: function(){
+                document.forms['order-form'].reset(); //очищаем форму
+                alert("Заказ Оформлен, проверьте Вашу почту");
+            },
+            error: function (){
+                alert("Заказ не оформлен, проверьте правильность заполнения формы!");
             }
-        })
-
-    })
+        });
+    });
 });
-// .done(function(data) {
-//     // var jsoned = JSON.parse(data);
-//     console.log(data);
-//     $('.result').html(data.login);
-//     $('.result2').html(data.password);
-// });
--- VP_1/sendler.php
+++ VP_1/sendler.php
@@ -2,64 +2,62 @@
 function clean_date($value)
 {
     $value = trim($value); // убираем пробелы
+//    $value = str_replace(" ", "", $value); //убираем все пробелы
     $value = stripslashes($value); //удаление экранированных символов
     $value = strip_tags($value); // удаление html и php тегов
     $value = htmlspecialchars($value); //спецсимволы в html сущности
     return $value;
 }
-$login = $_POST['name'];
-$mail = $_POST['email'];
-$phone = $_POST['phone'];
-$street = $_POST['street'];
-$home = $_POST['home'];
-$part = $_POST['part'];
-$flat = $_POST['appt'];
-$floor = $_POST['floor'];
-$comment = $_POST['comment'];
-$change = $_POST['payment'];
-$callback = $_POST['callback'];
-if (!empty($login) && !empty($mail) && !empty($phone) && !empty($street)
-    && !empty($home) && !empty($part) && !empty($flat) && !empty($floor)) {
-    $login = clean_date($login);
-    $mail = clean_date($mail);
-    $phone = clean_date($phone);
-    $street = clean_date($street);
-    $home = clean_date($home);
-    $part = clean_date($part);
-    $flat = clean_date($flat);
-    $floor = clean_date($floor);
-    if (empty($comment)) {
+if (!empty($_POST['name']) && !empty($_POST['email']) && !empty($_POST['phone']) && !empty($_POST['street'])
+     && !empty($_POST['home']) && !empty($_POST['appt']) && !empty($_POST['floor'])) {
+    $login = clean_date($_POST['name']);
+    $mail = clean_date($_POST['email']);
+    $phone = clean_date($_POST['phone']);
+    $street = clean_date($_POST['street']);
+    $home = clean_date($_POST['home']);
+    $part = clean_date($_POST['part']);
+    $flat = clean_date($_POST['appt']);
+    $floor = clean_date($_POST['floor']);
+    $comment = clean_date($_POST['comment']);
+    $change = clean_date($_POST['payment']);
+    $callback = clean_date($_POST['callback']);
+    if (!isset($callback)) {
+        $callback = 'Перезвонить';
+    } else {
+        $callback = 'Не перезванивать';
+    }
+    if (!isset($change) || $change == 'card') {
+        $change = 'По карте';
+    } else {
+        $change = 'Нужна сдача';
+    }
+    if (empty($comment)) { //если поле коммента пусто
         $comment = 'Комментария к заказу нет';
-    } else{
-        $comment = clean_date($comment);
     }
-    if (empty($change)) {
-        $change = '0';
+    if (empty($part) || $part == '0') {
+        $change = 'Корпуса нет';
     }
-    if (empty($callback)) {
-        $callback = '0';
+//    $login = 'ssss';
+//    $mail = 'S@SSS';
+//    $phone = '9649284';
+    try {
+        $pdo = new PDO('mysql:host=localhost;dbname=vp1;charset=utf8', 'root', '');
+        $stmt = $pdo->prepare("INSERT INTO Users (name,email,phone) VALUES (:login, :mail, :phone)");
+        $stmt->bindParam(':login', $login);
+        $stmt->bindParam(':mail', $mail);
+        $stmt->bindParam(':phone', $phone);
+        $stmt->execute();
+//        $idU = $pdo->query("SELECT id FROM Users WHERE email = '$mail'");
+//        $stmt = $pdo->prepare("INSERT INTO Orders (idUsers, needChang, callback, comment)
+//        VALUES (:iduser, :nchange, :callback, :comment)");
+//        $stmt->bindParam(':iduser', $idU);
+//        $stmt->bindParam(':nchange', $change);
+//        $stmt->bindParam(':callback', $callback);
+//        $stmt->bindParam(':comment', $comment);
+//        $stmt->execute();
+    } catch (PDOException $e) {
+        echo $e->getMessage();
     }
-    $str = "$login - $mail - $phone - $street - $home - $part - $flat - $floor - $comment - $change - $callback";
-    $handle = fopen('output.txt', "w+");
-    fwrite($handle, $str);
-    fclose($handle);
-} else {
-    $str = "Нечего записывать";
-    $handle = fopen('output.txt', "w+");
-    fwrite($handle, $str);
-    fclose($handle);
-    echo 'Не полностью заполнена форма';
 }
 
-//echo $login .'<br>';
-//echo $mail .'<br>';
-//echo $phone .'<br>';
-//echo $street .'<br>';
-//echo $home .'<br>';
-//echo $part .'<br>';
-//echo $flat .'<br>';
-//echo $floor .'<br>';
-//echo $comment .'<br>';
-//var_dump($callback);
-//echo '<br>';
-//var_dump($change);
+
